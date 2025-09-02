5ed107fe91ba34f351636626cbc65b2ae293ac52
Denis Vasilev <vassdeniss@gmail.com>
2025-09-03 01:05:34 +0300

    Add custom IQueryHandler

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -4,13 +4,9 @@ namespace BuildingBlocks.CQRS;
 
 public interface ICommandHandler<in TCommand>
     : IRequestHandler<TCommand, Unit>
-    where TCommand : ICommand<Unit>
-{
-}
+    where TCommand : ICommand<Unit>;
 
 public interface ICommandHandler<in TCommand, TResponse> 
     : IRequestHandler<TCommand, TResponse>
     where TCommand : ICommand<TResponse>
-    where TResponse : notnull
-{
-}
+    where TResponse : notnull;
