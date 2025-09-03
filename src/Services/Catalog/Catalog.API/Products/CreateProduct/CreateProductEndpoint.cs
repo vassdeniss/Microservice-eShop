e42e7a66f11150cba2481fea3380197b221cc425
Denis Vasilev <vassdeniss@gmail.com>
2025-09-03 19:09:19 +0300

    Implemented create product POST endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,30 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest req, ISender sender) =>
+        {
+            CreateProductCommand command = req.Adapt<CreateProductCommand>();
+            CreateProductResult result = await sender.Send(command);
+            CreateProductResult response = result.Adapt<CreateProductResult>();
+            
+            return Results.Created($"/products/{response.Id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,13 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 WebApplication app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
